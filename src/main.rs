8c346105af04543a12c857903b22350f7eaf9177
Smit Patil <smit@smit.codes>
2025-12-31 19:47:55 -0500

    Add hex color support

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
+use clap::Parser;
 use gif::{Encoder, Frame, Repeat};
 use image::{Rgb, RgbImage};
 use std::fs::File;
-use clap::Parser;
-
 
 #[derive(Parser)]
 #[command(author, version, about)]
@@ -14,7 +13,12 @@ struct Args {
     duration: u32,
 }
 
-fn parse_rgb(s: &str) -> Result<[u8; 3], String> {
+// We want use a [R,G,B] for our calculations
+fn parse_input(s: &str) -> Result<[u8; 3], String> {
+    if s.starts_with('#') {
+        return hex_to_rgb(s);
+    }
+
     let parts: Vec<&str> = s.split(',').collect();
     if parts.len() != 3 {
         return Err("Must be in R, G, B format".to_string());
@@ -24,6 +28,22 @@ fn parse_rgb(s: &str) -> Result<[u8; 3], String> {
     nums.map_err(|_| "Each value must be a number between 0 - 255".to_string())?
         .try_into()
         .map_err(|_| "Invalid RGB length".to_string())
+}
+
+fn hex_to_rgb(hex_code: &str) -> Result<[u8; 3], String> {
+    let hex_code = hex_code.trim_start_matches('#');
+
+    if hex_code.len() != 6 {
+        return Err("Hex code must be 6 characters long!".to_string());
+    }
+
+    let num = u32::from_str_radix(hex_code, 16).map_err(|_| "Invalid hex code!".to_string())?;
+
+    let r = ((num >> 16) & 0xFF) as u8;
+    let g = ((num >> 8) & 0xFF) as u8;
+    let b = (num & 0xFF) as u8;
+
+    Ok([r, g, b])
 }
 
 fn interpolate_color(color1: [u8; 3], color2: [u8; 3], ratio: f32) -> [u8; 3] {
@@ -57,24 +77,25 @@ fn generate_gradient_frame(
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     let args = Args::parse();
 
-    let color1 = parse_rgb(&args.start_color)?;
-    let color2 = parse_rgb(&args.end_color)?;
+    let color1 = parse_input(&args.start_color)?;
+    let color2 = parse_input(&args.end_color)?;
 
     let width = &args.width;
     let height = &args.height;
 
-
     let duration_seconds = &args.duration;
-    
+
     let fps = 30;
 
     let total_frames = duration_seconds * fps;
     let delay = (100.0 / fps as f32).round() as u16;
 
-    let file = File::create(format!("gradient{}x{}_{}.gif", width, height, duration_seconds))?;
+    let file = File::create(format!(
+        "gradient{}x{}_{}.gif",
+        width, height, duration_seconds
+    ))?;
     let mut encoder = Encoder::new(file, *width as u16, *height as u16, &[])?;
     encoder.set_repeat(Repeat::Infinite)?;
 
@@ -92,3 +113,4 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
